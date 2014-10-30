19501bfa37210978de4c856b8044ba47d51f9709
Stephen T Cannon <stephentcannon@gmail.com>
2014-10-30 15:39:32 -0400

    added versionf from

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   summary: "A collection of utilities added to the Meteor.Collection.prototype",
   // Version number.
-  version: "0.1.8",
+  version: "0.1.9",
   // Optional.  Default is package directory name.
   name: "steeve:meteor-collection-utils",
   // Optional github URL to your source repository.
@@ -9,7 +9,7 @@ Package.describe({
 });
 
 Package.onUse(function (api) {
-
+  api.versionsFrom('0.9.0');
   api.use('mongo@1.0.8');
   api.addFiles('Mongo.Collection.prototype.Utils.js', ['client', 'server']);
 });
