0c20ab59be9688a3dab2369b3990c5e9a645e14a
Stephen T Cannon <stephentcannon@gmail.com>
2014-10-30 15:13:43 -0400

    added mongo package to packages.js

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   summary: "A collection of utilities added to the Meteor.Collection.prototype",
   // Version number.
-  version: "0.1.6",
+  version: "0.1.8",
   // Optional.  Default is package directory name.
   name: "steeve:meteor-collection-utils",
   // Optional github URL to your source repository.
@@ -9,5 +9,7 @@ Package.describe({
 });
 
 Package.onUse(function (api) {
+
+  api.use('mongo@1.0.8');
   api.addFiles('Mongo.Collection.prototype.Utils.js', ['client', 'server']);
 });
